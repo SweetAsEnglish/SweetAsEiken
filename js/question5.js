994a6db2f06ec7cf39c9ff420b98534b080d97df
SimonStanbury <simon.stanbury@gmail.com>
2022-01-18 04:20:47 +0900

    Added first part Grade 5

---
+++ js/question5.js
@@ -0,0 +1,105 @@
+
+
+// Array of Objects
+const quiz = [
+{
+	q:'A: When do you usually have (&emsp;&emsp;&emsp;&emsp;)? <br> B: At 12:30. I eat it in my classroom.',
+	options:['lunch','hour','study','morning'],
+	answer: 0
+},
+{
+	q:'A: What color do you like? <br> B: I like (&emsp;&emsp;&emsp;&emsp;).',
+	options:['trees','music','black','birds'],
+	answer: 2
+},
+{
+	q:'A: I want a glass of (&emsp;&emsp;&emsp;&emsp;), Mom. <br> B: Here you are, Tom.',
+	options:['milk','meat','bread','fish'],
+	answer: 0
+},
+{
+	q:'A: Where is your dog? <br> B: He\'s (&emsp;&emsp;&emsp;&emsp;) the chair.',
+	options:['about','to','for','under'],
+	answer: 3
+},
+{
+	q:'A: Bye, Peter. <br> B: Bye, Sam. (&emsp;&emsp;&emsp;&emsp;) you tomorrow.',
+	options:['Read','Use','See','Open'],
+	answer: 2
+},
+{
+	q:'A: How (&emsp;&emsp;&emsp;&emsp;) is this swimming pool? <br> B: It\'s 50 meters.',
+	options:['old','cold','long','young'],
+	answer: 2
+},
+{
+	q:'A: Look (&emsp;&emsp;&emsp;&emsp;) that bird, Mom. <br> B: It\'s very pretty.',
+	options:['at','of','with','in'],
+	answer: 0
+},
+{
+	q:'A: (&emsp;&emsp;&emsp;&emsp;) phone is this? <br> B: It\'s Bill\'s.',
+	options:['When','Where','Whose','How'],
+	answer: 2
+},
+{
+	q:'Sam likes sports and can (&emsp;&emsp;&emsp;&emsp;) very fast.',
+	options:['run','sleep','see','rain'],
+	answer: 0
+},
+{
+	q:'Jack is on the school tennis (&emsp;&emsp;&emsp;&emsp;).',
+	options:['team','pool','box','home'],
+	answer: 0
+},
+{
+	q:'Lucy plays tennis (&emsp;&emsp;&emsp;&emsp;) school every day.',
+	options:['with','after','on','about'],
+	answer: 1
+},
+{
+	q:'My name is Linda Ford. I\'m (&emsp;&emsp;&emsp;&emsp;) Australia.',
+	options:['from','to','out','down'],
+	answer: 0
+},
+{
+	q:'I have two brothers. (&emsp;&emsp;&emsp;&emsp;) names are Ben and Steve.',
+	options:['My','Your','Our','Their'],
+	answer: 3
+},
+{
+	q:'Please (&emsp;&emsp;&emsp;&emsp;) quiet in the library.',
+	options:['be','is','am','are'],
+	answer: 0
+},
+{
+	q:'The eighth month of the year is (&emsp;&emsp;&emsp;&emsp;).',
+	options:['May','June','July','August'],
+	answer: 3
+},
+{
+	q:'Girl: Hi, my name is Sally. (&emsp;&emsp;&emsp;&emsp;) <br> Boy: My name is Bill.',
+	options:['What\'s your name?','How old are you?','Where\'s your house?','When\'s your birthday?'],
+	answer: 0
+},
+{
+	q:'Girl: Do you like my new skirt, Dad? <br> Father: (&emsp;&emsp;&emsp;&emsp;)',
+	options:['I\'m fine.','You\'re welcome.','Yes, it\'s nice.','No, it\'s not.'],
+	answer: 2
+},
+{
+	q:'Father: Who is the letter from, Judy? <br> Girl: (&emsp;&emsp;&emsp;&emsp;)',
+	options:['It\'s from Grandma.','I like her.','To the station.','In Tokyo.'],
+	answer: 0
+},
+{
+	q:'Girl 1: What does your sister do on Saturdays? <br> Girl 2: (&emsp;&emsp;&emsp;&emsp;)',
+	options:['Thank you very much.','She plays with her dog.','It\'s January 11.','It\'s in the afternoon.'],
+	answer: 3
+},
+{
+	q:'Woman: Are you a cook? <br> Man: (&emsp;&emsp;&emsp;&emsp;) I work at a Chinese restaurant',
+	options:['I\'m hungry.','I don\'t know.','Yes, please.','That\'s right.'],
+	answer: 3
+},
+]
