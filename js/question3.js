a750e00db9dde5ae25b6e94c97208327f94e6de6
SimonStanbury <simon.stanbury@gmail.com>
2022-01-19 04:35:57 +0900

    Grade 3 (20) Qs added

---
+++ js/question3.js
@@ -0,0 +1,105 @@
+
+
+// Array of Objects
+const quiz = [
+{
+	q:'I (&emsp;&emsp;&emsp;&emsp;) the music club at school, so I\'m going to buy a guitar this week.',
+	options:['learned','put','joined','kept'],
+	answer:2
+},
+{
+	q:'A: That\'s a (&emsp;&emsp;&emsp;&emsp;) T-shirt, Keisuke. <br> B: Thanks, Fred. I bought it at a concert last night.',
+	options:['cool','fast','silent','rich'],
+	answer: 0
+},
+{
+	q:'A: Tommy, turn off the TV and do your homework. <br> B: I\'ve (&emsp;&emsp;&emsp;&emsp;) finished it, Mom.',
+	options:['always','anytime','already','along'],
+	answer: 2
+},
+{
+	q:'A: How was your holiday, Steve? <br> B: Not so good. I wanted to go hiking, but it rained (&emsp;&emsp;&emsp;&emsp;) day.',
+	options:['any','some','all','long'],
+	answer: 2
+},
+{
+	q:'A: Do you like (&emsp;&emsp;&emsp;&emsp;) pictures, Mr. Fox? <br> B: Yes, I do.',
+	options:['take','took','takes','taking'],
+	answer: 3
+},
+{
+	q:'A: Do you have my book, Steve? <br> B: Sorry, I couldn\'t (&emsp;&emsp;&emsp;&emsp;) it all. I\'ll bring it on Monday.',
+	options:['read','reads','to read','reading'],
+	answer: 0
+},
+{
+	q:'Kento is very good at sports. He hopes to be a (&emsp;&emsp;&emsp;&emsp;) baseball player someday.',
+	options:['professional','first','delicious','traditional'],
+	answer:0
+},
+{
+	q:'It will (&emsp;&emsp;&emsp;&emsp;) you 10 minutes to get to the station by taxi.',
+	options:['make','fall','take','forget'],
+	answer:2
+},
+{
+	q:'Ken enjoys reading about different (&emsp;&emsp;&emsp;&emsp;). He hopes to travel to many countries in the future.',
+	options:['cultures','platforms','kitchens','purposes'],
+	answer:0
+},
+{
+	q:'You may not use your phone (&emsp;&emsp;&emsp;&emsp;) class. Please turn it off.',
+	options:['during','through','since','along'],
+	answer:0
+},
+{
+	q:'The station was full (&emsp;&emsp;&emsp;&emsp;) people because the trains were late.',
+	options:['by','for','of','on'],
+	answer:2
+},
+{
+	q:'Patricia is excited because she\'ll take part (&emsp;&emsp;&emsp;&emsp;) the school play this year.',
+	options:['on','to','at','in'],
+	answer:3
+},
+{
+	q:'Harry and Paul couldn\'t come (&emsp;&emsp;&emsp;&emsp;) with any good ideas for the science contest.',
+	options:['on','in','into','up'],
+	answer:3
+},
+{
+	q:'Our English teacher told us not to worry about making (&emsp;&emsp;&emsp;&emsp;) when we speak.',
+	options:['museums','mistakes','models','meetings'],
+	answer:1
+},
+{
+	q:'Katie\'s neighbor has a noisy dog, so it is difficult for her (&emsp;&emsp;&emsp;&emsp;) at night.',
+	options:['sleeps','to sleep','slept','sleep'],
+	answer:1
+},
+{
+	q:'Brother: Look! I made this cake for Mom\'s birthday. What do you think? <br> Sister: (&emsp;&emsp;&emsp;&emsp;) It looks delicous.',
+	options:['I\'m sure she\'ll love it.','I don\'t know where it is.','I\'m afraid I can\'t.','I\'ll tell her to call you.'],
+	answer: 0
+},
+{
+	q:'Husband: The kitchen is really dirty. <br> Wife: (&emsp;&emsp;&emsp;&emsp;) We need to clean it.',
+	options:['I haven\'t either.','There aren\'t any more.','I think so, too.','That\'s a long time.'],
+	answer: 2
+},
+{
+	q:'Girl 1: My aunt is taking me to a musical tomorrow. Would you like to come? <br> Girl 2: Sorry, I can\'t, but (&emsp;&emsp;&emsp;&emsp;)',
+	options:['thanks for asking.','I wrote that song.','she saw you there.','I don\'t know yet.'],
+	answer: 0
+},
+{
+	q:'Boy: You\'re an amazing swimmer, Jane. <br> Girl: Thanks. (&emsp;&emsp;&emsp;&emsp;) but I practiced a lot.',
+	options:['I wasn\'t good at first,','I was next to the pool,','You did it,','You should come with me,'],
+	answer: 0
+},
+{
+	q:'Husband: Let\'s walk to the shopping mall this afternoon. <br> Wife: OK. What do you need to buy? <br> Husband: (&emsp;&emsp;&emsp;&emsp;)',
+	options:['There\'s a bag on my desk.','I usually go by bus.','Let\'s leave at three o\'clock.','I want to look for a coat.'],
+	answer: 3
+}
+]
