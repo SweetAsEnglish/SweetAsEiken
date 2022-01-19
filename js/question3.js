8567bfd21386e49b00b56aaff97d8fbb575a9a29
SimonStanbury <simon.stanbury@gmail.com>
2022-01-19 13:28:50 +0900

    Update question3.js

--- js/question3.js
+++ js/question3.js
@@ -101,5 +101,105 @@ const quiz = [
 	q:'Husband: Let\'s walk to the shopping mall this afternoon. <br> Wife: OK. What do you need to buy? <br> Husband: (&emsp;&emsp;&emsp;&emsp;)',
 	options:['There\'s a bag on my desk.','I usually go by bus.','Let\'s leave at three o\'clock.','I want to look for a coat.'],
 	answer: 3
-}
+},
+{
+	q:'A: How did you make that potato dish? <br> B: First, you (&emsp;&emsp;&emsp;&emsp;) the potatoes, and then cut them in half and put butter on them.',
+	options:['boil','care','hurt','eat'],
+	answer: 0
+},
+{
+	q:'A: John, you should go to bed soon. If you stay up too late, you\'ll (&emsp;&emsp;&emsp;&emsp;) and be late for school. <br> B: OK, Mom.',
+	options:['graduate','promise','return','oversleep'],
+	answer: 3
+},
+{
+	q:'A: Did you buy your father something special for his birthday? <br> B: Yes. He loves to cook, so I got him a new (&emsp;&emsp;&emsp;&emsp;).',
+	options:['apron','ring','contact','field'],
+	answer: 0
+},
+{
+	q:'A: I told my mother that I would be home by 7:00. I don\'t want to (&emsp;&emsp;&emsp;&emsp;) my promise, so I have to go now. <br> B: OK.',
+	options:['pass','sell','break','lend'],
+	answer: 2
+},
+{
+	q:'A: Don\'t say anything to Dad about the surprise party! <br> B: Don\'t worry. He won\'t find (&emsp;&emsp;&emsp;&emsp;) about it from me.',
+	options:['within','through','out','near'],
+	answer: 2
+},
+{
+	q:'A: Do you like (&emsp;&emsp;&emsp;&emsp;) in Japan, Kent? <br> B: Yes, I do.',
+	options:['live','lived','lives','living'],
+	answer: 3
+},
+{
+	q:'A: Which shirt do you like? <br> B: I think the red one is  (&emsp;&emsp;&emsp;&emsp;) than the blue one.',
+	options:['nice','nicer','nicest','nicely'],
+	answer: 1
+},
+{
+	q:'Last summer, Hiroshi\'s family traveled around Japan. This year they want to go (&emsp;&emsp;&emsp;&emsp;).',
+	options:['abroad','inside','other','similar'],
+	answer:0
+},
+{
+	q:'Bob (&emsp;&emsp;&emsp;&emsp;) five friends to his party.',
+	options:['made','visited','invited','spoke'],
+	answer:2
+},
+{
+	q:'I bought a T-shirt for my brother, but I bought the wrong size. It was too (&emsp;&emsp;&emsp;&emsp;) for him.',
+	options:['heavy','clear','tight','bright'],
+	answer:2
+},
+{
+	q:'Sarah saw some flowers by the road while she was taking a walk. She (&emsp;&emsp;&emsp;&emsp;) a few and took them home.',
+	options:['spent','wished','picked','guessed'],
+	answer:2
+},
+{
+	q:'Jenny saw her grandparents (&emsp;&emsp;&emsp;&emsp;) the first time in years. She missed them very much.',
+	options:['for','from','out','over'],
+	answer:0
+},
+{
+	q:'My little sister is (&emsp;&emsp;&emsp;&emsp;) of the dark, so she always sleeps with my parents.',
+	options:['brave','true','glad','afraid'],
+	answer:3
+},
+{
+	q:'Joseph lost his glasses again. He searched all (&emsp;&emsp;&emsp;&emsp;) the place, but he couldn\'t find them.',
+	options:['over','under','up','down'],
+	answer:0
+},
+{
+	q:'Last Saturday, my bicycle was (&emsp;&emsp;&emsp;&emsp;). I have to buy a new one.',
+	options:['steal','stole','stolen','stealing'],
+	answer:2
+},
+{
+	q:'Girl 1: I\'m having a party from one on Saturday. (&emsp;&emsp;&emsp;&emsp;) <br> Girl 2: I\'d love to. I\'ll bring a pie.',
+	options:['I hope you can come.','I\'d like that please.','You missed it.','Your bus comes soon.'],
+	answer: 0
+},
+{
+	q:'Son: (&emsp;&emsp;&emsp;&emsp;) Mom? <br> Mother: First, wash these potatoes. Then, cut them into small pieces.',
+	options:['What did you cook for dinner,','What did you buy at the store,','What time will we have lunch,','What do you want me to do,'],
+	answer: 3
+},
+{
+	q:'Son: Mom, will the zoo be open on New Year\'s Day? <br> Mother: (&emsp;&emsp;&emsp;&emsp;) Let\'s check on the Internet.',
+	options:['Have a great year.','I\'m not sure.','It happened yesterday.','I did my best.'],
+	answer: 1
+},
+{
+	q:'Man: I\'m going skiing this Sunday. (&emsp;&emsp;&emsp;&emsp;) you should come with me. <br> Woman: That sounds fun. I\'d love to.',
+	options:['While you\'re at work,','Because your back hurts,','When you don\'t have time,','If you have no plans,'],
+	answer: 3
+},
+{
+	q:'Man: I went hiking in Nagano last Saturday. <br> Woman: That\'s pretty far from here. (&emsp;&emsp;&emsp;&emsp;) <br> Man: No, I came back to Tokyo that evening.',
+	options:['Do you know the way?','Did you take a map?','Do you want to go together?','Did you stay there?'],
+	answer: 3
+},
 ]
